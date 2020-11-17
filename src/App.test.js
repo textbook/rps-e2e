cc72167ace84646e9cd04f0d1e71c744ab157c66
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2020-11-17 21:52:20 +0000

    Implement integration test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe("App component", () => {
+  it("displays right wins when appropriate", () => {
+    // Arrange
+    const { getByLabelText, getByTestId, getByText } = render(<App />);
+
+    // Act
+    userEvent.selectOptions(getByLabelText("Left"), "paper");
+    userEvent.selectOptions(getByLabelText("Right"), "scissors");
+    userEvent.click(getByText("Throw"));
+
+    // Assert
+    expect(getByTestId("outcome")).toHaveTextContent("Right wins!");
+  });
 });
