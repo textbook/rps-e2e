3d94774a9fe465dba609f0ecea84256fc6a8b7e4
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2023-06-27 14:18:55 +0100

    Complete RPS UI implementation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Form from "./Form";
+import Outcome from "./Outcome";
+import { rps } from "./rpsService";
 
 function App() {
+  const [result, setResult] = useState();
+  
+  const onThrow = ([left, right]) => {
+    setResult(rps(left, right));
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form onSubmit={onThrow} />
+      {result && <Outcome result={result} />}
     </div>
   );
 }
